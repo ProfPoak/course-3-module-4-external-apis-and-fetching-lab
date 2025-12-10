70e94dbe25f0648f9fc549190642d2685611480a
Porter <pdhakes25@gmail.com>
2025-12-09 21:03:11 -0700

    fetch wip

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 const weatherApi = "https://api.weather.gov/alerts/active?area="
 
 // Your code here!
+
+function fetchWeatherAlerts() {
+    const stateAbv = document.getElementById("state-input")
+    
+    fetch(`weatherApi${stateAbv}`)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error("Error fetching data:", error))
+}
